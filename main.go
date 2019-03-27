bdfa6031449221587fa0dcc6a23fde745342b770
liuxuewen <liuxuewen@douyu.tv>
2019-03-27 23:12:25 +0800

    注释

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 func main() {
 
 	// fg.String主要工作
+	//1. 以value初始化了一个string指针
+	//2. 主要调用了fg.Var, 生成一个新的Flag,放入FlagSet的formal,  map[string]*Flag
 	stringPRet := fg.String("test", "testValue", "testUsage")
 
 	fmt.Println(*stringPRet) //在NewStringValue这里对p进行了赋值，所以返回指向3这个value的指针
